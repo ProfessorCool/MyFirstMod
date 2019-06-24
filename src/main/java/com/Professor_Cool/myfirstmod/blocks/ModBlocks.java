de7903c5e8446b18e9c86939b2ba6d074e08d341
ProfessorCool <professorcools@gmail.com>
2019-06-24 14:08:55 -0600

    Added first block

---
+++ src/main/java/com/Professor_Cool/myfirstmod/blocks/ModBlocks.java
@@ -0,0 +1,9 @@
+package com.Professor_Cool.myfirstmod.blocks;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+public class ModBlocks {
+
+    @ObjectHolder("myfirstmod:firstblock")
+    public static FirstBlock FIRSTBLOCK;
+}
